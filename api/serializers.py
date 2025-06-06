c188091adc81c4cadf1ea48423b047b3e7bdd510
Tuhin Chakrabarty <tuhinchakrabarty14@gmail.com>
2025-06-06 18:16:43 +0530

    Setup login

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+# api/serializers.py
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# api/views.py
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from .models import User
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
